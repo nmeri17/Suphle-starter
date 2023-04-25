4d1132c5f47c079d3d1c0c2e73073eefaa63eaeb
nmeri17 <vainglories17@gmail.com>
2023-04-25 13:06:20 +0100

    Add blade app shell

---
+++ ModuleTemplate/AllModules/_module_name/Markup/components/AppLayout.php
@@ -0,0 +1,18 @@
+<?php
+	namespace AllModules\_module_name\Markup\Components;
+
+	use Illuminate\View\Component;
+
+	class AppLayouts extends Component {
+
+		public function __construct ($pageTitle, $scripts) {
+
+			//
+		}
+
+		public function render () {
+
+			return view("layouts.app");
+		}
+	}
+?>
