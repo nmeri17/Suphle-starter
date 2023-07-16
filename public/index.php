a015e811b4a658afa86d7f3d3f9ae4c4d77eae41
nmeri17 <vainglories17@gmail.com>
2023-07-16 08:17:44 +0100

    Try read traditional request details from psr object

--- public/index.php
+++ public/index.php
@@ -4,10 +4,13 @@ use Suphle\Server\ModuleWorkerAccessor;
 
 use AllModules\PublishedModules;
 
+use GuzzleHttp\Psr7\ServerRequest;
+
 require_once "../vendor/autoload.php";
 
 echo (new ModuleWorkerAccessor(new PublishedModules(), true))
 
-->buildIdentifier()->getRequestRenderer($_GET["suphle_path"], true)
+->buildIdentifier()->getRequestRenderer(
 
-->render();
+	$_GET["suphle_path"], true, ServerRequest::fromGlobals()
+)->render();
